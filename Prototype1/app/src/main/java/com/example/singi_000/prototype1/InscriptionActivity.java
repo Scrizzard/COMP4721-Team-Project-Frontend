fedb8eba9e684676132ccd104dd18ca242e1e546
Corinne Madsen <camadsen@mta.ca>
2015-03-04 16:53:22 -0400

    Working Inscription Text File Saving
    The Inscription Tabs now work completely. None are open at the
    beginning, but one can add and close test tabs and then switch to other
    activities or leave the app, and the same set of inscription tabs with
    the same selected one will open when the inscription page is reopened.

--- Prototype1/app/src/main/java/com/example/singi_000/prototype1/InscriptionActivity.java
+++ Prototype1/app/src/main/java/com/example/singi_000/prototype1/InscriptionActivity.java
@@ -12,14 +12,15 @@ import android.widget.TabHost;
 import android.view.View;
 
 import com.example.singi_000.testmultiplescreens.R;
-
+import java.io.*;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class InscriptionActivity extends BaseActivity {
 
 
     private final static int MAX_TABS = 8;
     private int[] tabInscriptionKeys=new int[MAX_TABS];
-    private boolean[] tabOpen=new boolean[MAX_TABS];
     private Button[] tabs=new Button[MAX_TABS];
     private int currTab=0;
     private int numTabsOpen;
@@ -47,23 +48,115 @@ public class InscriptionActivity extends BaseActivity {
             });
         }
 
-        //here is where I need to add loading in from file
+        loadTabs();
+        for(int i=0; i<MAX_TABS; i++){
+            if(i<numTabsOpen) {
+                tabs[i].setVisibility(View.VISIBLE);
+            }
+            else{
+                tabs[i].setVisibility(View.INVISIBLE);
+            }
+        }
+        switchTab(currTab);
 
-        tabs[currTab].setBackgroundColor(Color.BLUE);
 
-        //filling up with example inscription numbers
+        //setting up the current colors of the tabs for testing
         for(int i=0; i<MAX_TABS; i++){
             tabs[i].setBackgroundColor(Color.LTGRAY);
-            tabInscriptionKeys[i]=i+20;
-            tabOpen[i]=true;
         }
-        numTabsOpen=8;
+        tabs[currTab].setBackgroundColor(Color.BLUE);
+
+
 
 
     }
 
+    /**
+     * This method saves the current state of the tabs (which inscription tabs are open and which
+     * one is currently selected) to a text file that can be read to reopen the tabs when switching
+     * between activities or reopening the app.
+     */
+    public void saveTabs(){
+        try {
+            FileOutputStream fileout=openFileOutput("InscriptionData.txt", MODE_PRIVATE);
+            OutputStreamWriter outputWriter=new OutputStreamWriter(fileout);
+            outputWriter.write(tabInscriptionKeys[currTab] + "\n");
+            for (int i = numTabsOpen-1; i >=0; i--) {
+                outputWriter.write(tabInscriptionKeys[i] + "\n");
+            }
+            outputWriter.close();
+        }catch(IOException e){
+            //This shouldn't happen since the file does exist and is writable.
+            //I'm not sure what sort of error handling should be placed here.
+        }
+    }
+
+    public void loadTabs(){
+        try {
+            FileInputStream fileIn=openFileInput("InscriptionData.txt");
+            InputStreamReader inputRead= new InputStreamReader(fileIn);
+            int firstChar=inputRead.read();
+            if(firstChar==-1){
+                //the file is empty, so there are no tabs open
+                numTabsOpen=0;
+                return;
+            }
+            char currChar=(char)firstChar;
+            String st = "";
+            while(currChar!='\n'){
+                st+=currChar;
+                currChar=(char)inputRead.read();
+            }
+            int currTabInscriptionKey=Integer.valueOf(st);
+            Stack<Integer> keys = new Stack<Integer>();
+            boolean done=false;
+            while(!done){
+                int readChar=inputRead.read();
+                if(readChar==-1){
+                    done=true;
+                }
+                else {
+                    currChar = (char) readChar;
+                    st = "";
+                    while (currChar != '\n') {
+                        st += currChar;
+                        currChar = (char) inputRead.read();
+                    }
+                    keys.push(Integer.valueOf(st));
+                }
+            }
+            inputRead.close();
+
+            //first, figuring out if the currTab will be one of the 8 open, if not making it 0
+            currTab=keys.search(currTabInscriptionKey)-1;
+            if(currTab>7){
+                currTab=0;
+            }
+            numTabsOpen=0;
+            for(int i=0; i<MAX_TABS; i++){
+                if(!keys.empty()) {
+                    numTabsOpen++;
+                    tabInscriptionKeys[i] = keys.pop();
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("FILE NOT FOUND ");
+            //This shouldn't happen since the file does exist and is writable.
+            //I'm not sure what sort of error handling should be placed here.
+        }
+
+
+
+    }
+
+    /**
+     * This method responds to the click of a specific tab by switching all of the Inscription
+     * fields to those of the Inscription corresponding to that tab. It also switches which tab
+     * button is currently highlighted.
+     * @param tabNum - the number corresponding to the tab clicked
+     */
     public void switchTab(int tabNum){
-        //this method will go through and switch out all of the inscription stuff to the next one
         //for now, it just switches the text field to the current tab number
         tabs[currTab].setBackgroundColor(Color.LTGRAY);
         currTab=tabNum;
@@ -71,61 +164,77 @@ public class InscriptionActivity extends BaseActivity {
         TextView t=new TextView(this);
         t=(TextView)findViewById(R.id.tabExampleText);
         t.setText(""+tabInscriptionKeys[tabNum]);
+        saveTabs();
     }
 
+    /**
+     * This method opens up a new tab for an inscription by its key. If the inscription is already
+     * open, it just moves it to another tab. Otherwise, it adds it to the top and moves all other
+     * tabs down, possibly removing the bottom tab if there are already the maximum number of tabs.
+     *
+     * @param inscriptionKey - the key of the inscription to be opened
+     */
     public void openTab(int inscriptionKey){
         //making sure this inscription isn't open yet
-        for(int i=0; i<MAX_TABS; i++){
+        for(int i=0; i<numTabsOpen; i++){
             if(tabInscriptionKeys[i]==inscriptionKey){
                 //the tab is already open, so we just want to move it to the top
                 for(int j=i; j>0; j--){
                     tabInscriptionKeys[j]=tabInscriptionKeys[j-1];
-                    tabOpen[j]=tabOpen[j-1];
                 }
                 tabInscriptionKeys[0]=inscriptionKey;
-                tabOpen[0]=true;
+                saveTabs();
                 return;
             }
         }
         for(int i=MAX_TABS-1; i>0; i--){
             tabInscriptionKeys[i]=tabInscriptionKeys[i-1];
-            tabOpen[i]=tabOpen[i-1];
         }
         tabInscriptionKeys[0]=inscriptionKey;
-        tabOpen[0]=true;
         if(numTabsOpen<MAX_TABS){
             numTabsOpen++;
             tabs[numTabsOpen-1].setVisibility(View.VISIBLE);
         }
         switchTab(0);
+        saveTabs();
     }
 
+    /**
+     * THIS METHOD WILL NOT EXIST IN LATER VERSIONS. It is here to test the opening of tabs.
+     * @param view
+     */
     public void testTabOpen(View view) {
         openTab(testTabNum++);
     }
 
+    /**
+     * This method allows a tab to be closed. It moves up all tabs below it and removes the current
+     * inscription tab.
+     * @param view
+     */
     public void closeTab(View view){
         for(int i=currTab; i<MAX_TABS-1; i++){
             tabInscriptionKeys[i]=tabInscriptionKeys[i+1];
-            tabOpen[i]=tabOpen[i+1];
         }
         tabInscriptionKeys[MAX_TABS-1]=-1;
-        tabOpen[MAX_TABS-1]=false;
-        for(int i=0; i<MAX_TABS; i++){
-            if(!tabOpen[i]){
-                tabs[i].setVisibility(View.INVISIBLE);
-            }
-        }
-        if(tabOpen[currTab]) {
+        numTabsOpen--;
+         tabs[numTabsOpen].setVisibility(View.INVISIBLE);
+        if(currTab<numTabsOpen){
             switchTab(currTab);
         }
         else{
-            //checking to make sure there's a tab left;
-            if(currTab-1>-1) {
-                switchTab(currTab - 1);
+            if(currTab>0) {
+                currTab--;
+                switchTab(currTab);
+            }
+            else{
+                //FILL IN CODE HERE TO HANDLE NO MORE INSCRIPTION TABS BEING OPEN
+                //All tabs have been closed;
+                //Fill all inscription fields with nothing
+                //Make title "No Inscriptions Open"
             }
         }
-        numTabsOpen--;
+        saveTabs();
     }
 
     //---------------------------------------------------------------//
